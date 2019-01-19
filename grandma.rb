acab790b1fa78570864c29cce0606ed5a6aec627
yong liang <yongliang24@gmail.com>
2019-01-19 01:25:26 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -16,9 +16,12 @@ def speak_to_grandma (phrase)
   if phrase != phrase.upcase
     return "HUH?! SPEAK UP, SONNY!"
 
+elsif phrase == phrase.upcase
+  return "I LOVE YOU TOO PUMPKIN!"
+  
   else
     return "NO, NOT SINCE 1938!"
-    return "I LOVE YOU TOO PUMPKIN!"
+
   end
 
 end
